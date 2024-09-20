fbe63ff779295e873790658efd3a7fe1c60e2939
midCenturyDude7 <mgriffes@gmail.com>
2024-09-19 20:30:33 -0700

    Updated styles and edited code

--- app/components/header/Header.jsx
+++ app/components/header/Header.jsx
@@ -3,16 +3,28 @@ import React from "react";
 import Nav from "./nav/Nav";
 import { AnimatePresence } from "framer-motion";
 import styles from "./style.module.scss";
+import { usePathname } from "next/navigation";
 
 export default function Header() {
   const [isActive, setIsActive] = React.useState(false);
+  const pathname = usePathname();
+
+  React.useEffect(() => {
+    if (isActive) setIsActive(false);
+  }, [pathname]);
 
   return (
     <>
-      <div onClick={() => setIsActive(!isActive)} className={styles.button}>
-        <div
-          className={`${styles.burger} ${isActive ? styles.burgerActive : ""}`}
-        />
+      <div>
+        <div className={styles.header}>
+          <div onClick={() => setIsActive(!isActive)} className={styles.button}>
+            <div
+              className={`${styles.burger} ${
+                isActive ? styles.burgerActive : ""
+              }`}
+            />
+          </div>
+        </div>
       </div>
       <AnimatePresence mode="wait">{isActive && <Nav />}</AnimatePresence>
     </>
